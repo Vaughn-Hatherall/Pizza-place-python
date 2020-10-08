553d1a16613e776a257049a6fcd41a78db10e6dd
Vaughn-Hatherall <71399950+Vaughn-Hatherall@users.noreply.github.com>
2020-10-08 14:28:50 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+width=input("Ok and how wide do you want that pizza (inches)")
+width=int(width)
+subtotal1=width*0.5
+totalsubtotal=subtotal1+1+.75
+totaldollars=totalsubtotal*1.13
+print ('the total of the pizza is')
+print (totaldollars)
